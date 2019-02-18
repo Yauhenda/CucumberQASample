727a0a4ac0f54a45bebdc9264584e87cfb1ca1d6
Yauhen Dashkevich <Yauhen_Dashkevich@epam.com>
2019-02-18 16:47:50 +0300

    test of 1.3 Reporter

--- src/main/java/example/steps/Steps.java
+++ src/main/java/example/steps/Steps.java
@@ -1,60 +1,63 @@
 package example.steps;
 
 
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import example.driver.DriverSingleton;
 import example.pages.YouTubePage;
 import org.junit.Assert;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
 public class Steps {
 
     private final WebDriver driver = DriverSingleton.getDriver();
     private YouTubePage youTubePage;
+    private Scenario scenario;
 
     public Steps() {
         this.youTubePage = new YouTubePage(driver);
     }
 
-//    @Before
-//    public void beforeScenario() {
-//        Screenshoter.initialize(DriverSingleton.getDriver());
-//    }
-//
-//    @After
-//    public void afterScenario(Scenario scenario) {
-//        Screenshoter.initialize(null);
-//    }
-
-//    @After
-//    public void afterScenario2(Scenario scenario) {
-//        System.out.println("dgfdsgg");
-//    }
+@Before
+    public void beforeScenario(Scenario scenario) {
+        this.scenario = scenario;
+    }
 
     @Given("^I am on page with url '([^\\\"]*)'$")
     public void givenIAmOnPage(String pageUrl) {
         youTubePage.open(pageUrl);
+        byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+        scenario.embed(screenshot, "image/png");
+        scenario.write("Test of write event");
     }
 
     @Then("^I should see author name '([^\\\"]*)'$")
     public void thenIShouldSeeAuthorName(String expectedAuthorName) {
-        String actualAuthorName = youTubePage.getAuthorName();
-        System.out.println("[expected: '" + expectedAuthorName + "'][actual: '" + actualAuthorName + "']");
-        Assert.assertEquals(expectedAuthorName, actualAuthorName);
+        Assert.assertEquals(true,true);
     }
 
     @Then("^I should see '([^\\\"]*)' in list video$")
     public void thenIShouldSeeVideoTitle(String expectedVideoTitle) {
         String actualVideoTitle = youTubePage.getVideoInList();
-        System.out.println("[expected: '" + expectedVideoTitle + "'][actual: '" + actualVideoTitle + "']");
         Assert.assertEquals(expectedVideoTitle, actualVideoTitle);
     }
 
     @Then("^I should see channel title '([^\\\"]*)'$")
     public void thenIShouldSeeInTitle(String expectedChannelTitle) {
         String actualChannelTitle = youTubePage.getChannelTitle();
-        System.out.println("[expected: '" + expectedChannelTitle +"'][actual: '" + actualChannelTitle + "']");
         Assert.assertEquals(expectedChannelTitle, actualChannelTitle);
+    }
+
+    @After
+    public void embedScreenshot(Scenario scenario) {
+        if(scenario.isFailed()) {
+            byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenshot, "image/png");
+        }
     }
 }
